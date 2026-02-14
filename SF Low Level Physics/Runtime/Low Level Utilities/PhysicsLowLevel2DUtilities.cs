d1e2c110e9711c79a37d6f1fecd8e9e43e18cc1b
8BitsPerByte <jonathan.westfall1@gmail.com>
2026-02-14 17:35:23 -0600

    Added some utility methods for PhysicsWorld related queries and contact filters.

--- SF Low Level Physics/Runtime/Low Level Utilities/PhysicsLowLevel2DUtilities.cs
+++ SF Low Level Physics/Runtime/Low Level Utilities/PhysicsLowLevel2DUtilities.cs
@@ -1,4 +1,4 @@
-using System;
+using Unity.Collections;
 using UnityEngine;
 using UnityEngine.LowLevelPhysics2D;
 
@@ -157,5 +157,82 @@ namespace SF.PhysicsLowLevel
 
             return null;
         }
+
+#region World Queries
+        public static NativeArray<PhysicsQuery.WorldOverlapResult> OverlapOnWorld(
+            this PhysicsShape shape,
+            PhysicsWorld world,
+            PhysicsQuery.QueryFilter filter,
+            Allocator allocator = Allocator.Temp)
+        {
+            PhysicsShape.ShapeType shapeType = shape.shapeType;
+            NativeArray<PhysicsQuery.WorldOverlapResult> nativeArray;
+            switch (shapeType)
+            {
+                case PhysicsShape.ShapeType.Circle:
+                    nativeArray = world.OverlapGeometry(shape.circleGeometry.Transform(shape.body.transform), filter, allocator);
+                    break;
+                case PhysicsShape.ShapeType.Capsule:
+                    nativeArray = world.OverlapGeometry(shape.capsuleGeometry.Transform(shape.body.transform), filter, allocator);
+                    break;
+                case PhysicsShape.ShapeType.Segment:
+                    nativeArray = world.OverlapGeometry(shape.segmentGeometry.Transform(shape.body.transform), filter, allocator);
+                    break;
+                case PhysicsShape.ShapeType.Polygon:
+                    nativeArray = world.OverlapGeometry(shape.polygonGeometry.Transform(shape.body.transform), filter, allocator);
+                    break;
+                case PhysicsShape.ShapeType.ChainSegment:
+                    nativeArray = world.OverlapGeometry(shape.chainSegmentGeometry.segment.Transform(shape.body.transform), filter, allocator);
+                    break;
+                default:
+                    throw new System.ArgumentException("Invalid shape type used.", nameof (shape));
+            }
+            return nativeArray;
+        }
+#endregion
+
+        public static PhysicsQuery.QueryFilter GetQueryFilter(this in PhysicsShape.ContactFilter contactFilter) 
+            => new PhysicsQuery.QueryFilter(contactFilter.categories, contactFilter.contacts);
+        
+        public static PhysicsQuery.QueryFilter GetQueryFilter(this in PhysicsShape shape) 
+            => new PhysicsQuery.QueryFilter(shape.contactFilter.categories, shape.contactFilter.contacts);
+
+        
+        /// <summary>
+        /// Only works when the <see cref="PhysicsShape.shapeType"/> is
+        /// a <see cref="PhysicsShape.ShapeType.Capsule"/>
+        /// </summary>
+        /// <param name="shape"></param>
+        /// <param name="world"></param>
+        /// <param name="targetPosition"></param>
+        /// <param name="velocity"></param>
+        /// <param name="maxSolverIterations"></param>
+        /// <param name="moveTolerance"></param>
+        /// <returns></returns>
+        public static PhysicsQuery.WorldMoverResult CastMover(
+            this in PhysicsShape shape,
+            in PhysicsWorld world,
+            Vector2 targetPosition,
+            Vector2 velocity,
+            int maxSolverIterations = 1,
+            float moveTolerance = 0.01f
+            )
+        {
+            
+            var worldMoveResult = new PhysicsQuery.WorldMoverInput
+            {
+                geometry = shape.capsuleGeometry,
+                maxIterations = maxSolverIterations,
+                overlapFilter = shape.GetQueryFilter(),
+                castFilter =shape.GetQueryFilter(),
+                moveTolerance = moveTolerance,
+                targetPosition = targetPosition,
+                transform = shape.transform,
+                velocity = velocity
+            };
+
+            return world.CastMover(worldMoveResult);
+        }
     }
+    
 }
