721cf994a6370916f8d3cd358d9efde7530cb391
8BitsPerByte <jonathan.westfall1@gmail.com>
2026-02-19 16:14:02 -0600

    Updated: Improved the PlayerInteractionController by implementing a new set of defined const int for PhysicsLayer.

--- Runtime/Interactables/PlayerInteractionController.cs
+++ Runtime/Interactables/PlayerInteractionController.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using Unity.Collections;
 using UnityEngine.LowLevelPhysics2D;
-using ZTDR.PhysicsLowLevel;
 
 namespace SF.Interactables
 {
@@ -16,53 +15,45 @@ namespace SF.Interactables
         private void Awake()
         {
             TryGetComponent(out _controller);
-            TryGetComponent(out _hitShape);
+            if(_hitShape == null)
+                TryGetComponent(out _hitShape);
         }
         
               
         private void OnEnable()
         {
+            SFInputManager.Controls.Player.Enable();
             SFInputManager.Controls.Player.Interact.performed += OnInteractPerformed;
-            if(_hitShape != null)
+            SFInputManager.Controls.Player.Interact.started += OnInteractPerformed;
+
+            if (_hitShape != null)
+            {
                 _hitShape.AddTriggerCallbackTarget(this);
+                _hitShape.Shape.contactFilter.contacts.SetBit(SFPhysicsManager.InteractableLayer);
+                _hitShape.Body.enabled  = false;
+            }
         }
 
         private void OnDisable()
         {
+            if(SFInputManager.Controls == null) return;
+            
             SFInputManager.Controls.Player.Interact.performed -= OnInteractPerformed;
+            SFInputManager.Controls.Player.Interact.started   -= OnInteractPerformed;
             if(_hitShape != null)
                 _hitShape.RemoveTriggerCallbackTarget(this);
         }
-
-
-        public void OnTriggerBegin2D(PhysicsEvents.TriggerBeginEvent beginEvent)
-        {
-            // Grab the body data.
-            var objectData = beginEvent.triggerShape.body.userData.objectValue;
-            
-            if (objectData is GameObject hitObject
-                && hitObject.TryGetComponent(out IInteractable interactable)
-                && interactable.InteractableMode == InteractableMode.Collision)
-            {
-                if(interactable is IInteractable<PlayerControllerBody2D> interactableController
-                   && _controller is not null)
-                    interactableController.Interact(_controller);
-                else
-                    interactable.Interact();
-            }
-        }
-
-        public void OnTriggerEnd2D(PhysicsEvents.TriggerEndEvent endEvent) { }
-
+        
         protected void OnInteractPerformed(InputAction.CallbackContext ctx)
         {
             if(!_hitShape.Shape.isValid) return;
-
-            _hitShapes            = new NativeArray<PhysicsShape>(5, Allocator.Temp);
-            _castInput.shapeProxy = _hitShape.ShapeProxy;
+            _hitShape.Body.position = transform.position;
+            _hitShape.Body.enabled  = true;
+            _hitShapes              = new NativeArray<PhysicsShape>(5, Allocator.Temp);
             using var result = _hitShape.PhysicsWorld.OverlapShape(_hitShape.Shape, _interactableFilter);
-
-            if (result.Length < 0)
+            _hitShape.Body.enabled  = false;
+            
+            if (result.Length < 1)
                 return;
 
             // This is a painful looking thing, but it is actually decent performance, so oh well.
@@ -86,7 +77,19 @@ namespace SF.Interactables
   
         public void OnTriggerBegin2D(PhysicsEvents.TriggerBeginEvent beginEvent, SFShapeComponent callingShapeComponent)
         {
-            OnTriggerBegin2D(beginEvent);
+            // Grab the body data.
+            var objectData = beginEvent.triggerShape.body.userData.objectValue;
+            
+            if (objectData is GameObject hitObject
+                && hitObject.TryGetComponent(out IInteractable interactable)
+                && interactable.InteractableMode == InteractableMode.Collision)
+            {
+                if(interactable is IInteractable<PlayerControllerBody2D> interactableController
+                   && _controller is not null)
+                    interactableController.Interact(_controller);
+                else
+                    interactable.Interact();
+            }
         }
 
         public void OnTriggerEnd2D(PhysicsEvents.TriggerEndEvent endEvent, SFShapeComponent callingShapeComponent)
