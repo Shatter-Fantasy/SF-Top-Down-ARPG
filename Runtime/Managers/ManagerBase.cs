fc92cb4c81a758c26da4f6b12ee08f123fc3eab9
8BitsPerByte <jonathan.westfall1@gmail.com>
2026-04-27 14:27:51 -0500

    Added a ManagerBaseStaticCleanUp that just cleans up static values when entering playmode with domain reload turned off.

--- Runtime/Managers/ManagerBase.cs
+++ Runtime/Managers/ManagerBase.cs
@@ -1,9 +1,12 @@
+using Unity.Scripting.LifecycleManagement;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace SF
 {
     public abstract class ManagerBase<T> : MonoBehaviour
     {
+        [NoAutoStaticsCleanup]
         protected static T _instance;
 
         public static T  Manager
@@ -12,5 +15,17 @@ namespace SF
             set => _instance = value;
         }
         
+    }
+    
+    public abstract partial class ManagerBaseStaticCleanUp<T> : MonoBehaviour
+    {
+        [AutoStaticsCleanup]
+        protected static T _instance;
+
+        public static T  Manager
+        {
+            get => _instance;
+            set => _instance = value;
+        }
     }
 }
